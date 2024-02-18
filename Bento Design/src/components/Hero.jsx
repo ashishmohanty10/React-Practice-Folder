3f0d9e0034a5f55c939dc110be28e9d95cd91d20
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-02-18 21:20:54 +0530

    bento done

--- Bento Design/src/components/Hero.jsx
+++ Bento Design/src/components/Hero.jsx
@@ -1,46 +1,48 @@
 import React from "react";
+
 import HeroImg from "/heroimg.png";
 import Clock from "/clock.png";
 import Face from "/face.svg";
 
 const Hero = () => {
   return (
-    <div className="py-1">
+    <div className="py-5">
       <div className="container">
         {/* first section */}
-        <div className="lg:flex justify-start items-baseline gap-5">
-          <h1 className="lg:text-[200px] text-9xl font-medium lg:text-left text-center">
-            superb
-          </h1>
-          <p className="lgs:w-[20%] font-medium text-center lgs:text-left my-4 lg:my-0">
-            my new design agency that is definitly not a design subscription
-            agency.
-          </p>
+        <div className="lg:grid grid-cols-12 py-5">
+          <div className="sm:col-span-8 lg:flex justify-between items-baseline">
+            <h1 className="lg:text-9xl font-medium text-5xl text-center lg:text-left ">
+              superb
+            </h1>
+
+            <p className="lg:w-[30%] w-full text-center lg:text-left">
+              My new design agency that is definitly not a design subscription
+              agency.
+            </p>
+          </div>
         </div>
 
-        {/* second section */}
-
-        <div className="lg:flex justify-between items-center gap-5">
-          <div>
-            <img src={HeroImg} alt="" className="rounded-3xl w-auto" />
+        {/* second div */}
+        <div className="lg:grid grid-cols-12 gap-5 m-5">
+          <div className="lg:col-span-8 rounded-3xl overflow-hidden mb-5 lg:mb-0">
+            <img src={HeroImg} alt="" className="w-full h-full " />
           </div>
 
-          <div className="w-full lg:w-[25%] flex flex-col justify-between gap-y-5 my-4 lg:my-0">
-            <div className="h-1/2 bg-[#F7A072] p-5 flex flex-col items-start justify-between gap-10 rounded-3xl">
-              <img src={Clock} alt="" />
+          <div className="lg:col-span-4 flex flex-col gap-4">
+            <div className="h-1/2 bg-box1 rounded-3xl p-5 flex flex-col justify-between gap-10 mb-5 lg:mb-0">
+              <img src={Clock} alt="" className="w-[40px]" />
 
-              <p className="w-[60%]">
+              <p>
                 always on time. because we respect you and know that time is
                 money.
               </p>
             </div>
 
-            <div className="h-1/2 bg-[#B5E2FA] p-5 flex flex-col items-start justify-between gap-10 rounded-3xl">
-              <img src={Face} alt="" />
+            <div className="h-1/2 bg-box2 rounded-3xl p-5 flex flex-col justify-between gap-10 mb-5 lg:mb-0">
+              <img src={Face} alt="" className="w-[40px]" />
 
-              <p className="w-[60%]">
-                always on time. because we respect you and know that time is
-                money.
+              <p>
+                You’ll leave happy. because we’re hear to make sure you win.
               </p>
             </div>
           </div>
